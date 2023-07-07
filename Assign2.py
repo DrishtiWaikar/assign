d3403c561f34ab243692b64e3337753bdd0cb320
DrishtiWaikar <136799495+DrishtiWaikar@users.noreply.github.com>
2023-07-07 21:33:35 +0530

    Add files via upload

---
+++ Assign2.py
@@ -0,0 +1,71 @@
+#Arithmetic Operator 
+print("Arithmetic Operator")
+a=10
+b=5
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Floor Division", a//b)
+print("Modulus",a%b)
+print("Exponent:", a**b)
+
+#Assignment Operator 
+print("Assignment Operator")
+a=10
+print("value of a is:",a)
+a+=2
+print("+=",a)
+a-=4
+print("-=",a)
+a*=6
+print("*=",a)
+a/=8
+print("/=",a)
+a%=10
+print("%=",a)
+a//=12
+print("//=",a)
+
+#Comparison Operator 
+print("Comparison Operator")
+c=20
+d=10
+print("c<d?",c<d)
+print("c>d?",c>d)
+print("c<=d?",c<=d)
+print("c>=d?",c>=d)
+print("c==d?",c==d)
+print("c!=d?",c!=d)
+
+#Logical Operator 
+print("Logical Operator")
+m=True
+n=False 
+print("m and n =", m and n)
+print("m or n =", m or n)
+print("not n =", not n)
+
+#Bitwise Operator 
+print("Bitwise Operator")
+p=4
+q=8
+print("p&q=",p&q)
+print("p|q=",p|q)
+print("p^q=",p^q)
+print("p>>q=",p>>q)
+print("p<<q=",p<<q)
+
+#Identity Operator 
+print("Identity Operator")
+p=4
+q=8
+print("p is q?", p is q)
+print("p id not q ?", p is not q)
+
+#Membership Operator 
+print("Membership Operator")
+x=("Drishti")
+print("d" in x)
+print("a" not in x)
+
